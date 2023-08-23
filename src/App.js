1836b91dbcc4e9a19b74fe5e29bd4a33e56c0023
Noor Mohammad <786mrnoor@gmail.com>
2023-08-23 21:57:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './component/Home'
 import Header from './component/Header'
 import About from './component/About'
@@ -8,7 +8,7 @@ import Error from './component/Error'
 
 function App() {
   return (
-    <BrowserRouter basename='react-router' >
+    <HashRouter basename='/react-router' >
       <Routes>
         <Route path='/' element={<Header />}>
           <Route index element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
