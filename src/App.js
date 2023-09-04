2a7d07c5c06aede7e7047cc5f3584d281a674bd5
Noor Mohammad <786mrnoor@gmail.com>
2023-09-04 09:52:27 +0530

    remove BaseName

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Header />}>
           <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
